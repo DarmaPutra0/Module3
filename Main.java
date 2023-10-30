99956fee9ea9923450807c5e2a22ab9c6cdc444d
asus- <darmaputra443@gmail.com>
2023-10-30 11:02:06 +0700

    add Java Doc

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
+
     public static void main(String[] args) {
+    /**
+     * @ param
+     * */
+
+    /**@ return
+     *
+     * */
         System.out.println("Hello,Git!");
     }
 }
